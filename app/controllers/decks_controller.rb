19c73236af300a6e4d828ab5b131a5335b1abc5e
Samuel Weisse <samuelweisse@Generals-MacBook-Air-5.local>
2016-08-13 19:16:36 -0700

    full crud status WOOT

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -15,8 +15,26 @@ class DecksController < ApplicationController
     redirect_to decks_path;
   end
 
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+
+  def update
+    deck = Deck.find(params[:id])
+    deck.update(deck_params)
+
+    redirect_to "/cards?deck_id="+params[:id]
+  end
+
+  def destroy
+    deck = Deck.find(params[:id])
+    deck.destroy
+
+    redirect_to decks_path
+  end
+
   private
   def deck_params
-    params.require(:deck).permit(:name, :type, :user_id)
+    params.require(:deck).permit(:name, :type, :user_id, :character_class)
   end
 end
